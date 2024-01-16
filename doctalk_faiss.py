c9df434ed67bd96318402caf5f43186c847b13ff
ishu-knit <ishu.20226@knit.ac.in>
2024-01-17 00:30:32 +0530

    3rd commit

--- doctalk_faiss.py
+++ doctalk_faiss.py
@@ -1,89 +1,59 @@
-# successfull running
 
-# for downloading a pdf -file from the internet
-
-# import requests
-# url = "https://www.impromptubook.com/wp-content/uploads/2023/03/impromptu-rh.pdf"
-# response = requests.get(url)
-# with open("impromptu-rh.pdf", "wb") as pdf_file:
-#     pdf_file.write(response.content)
-# print("Download complete.")
-
-
-
+import streamlit as st
 from PyPDF2 import PdfReader
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-
-
-# location of the pdf .
-doc_reader = PdfReader('impromptu-rh.pdf')
-
-
-# read data from the file and put them into a variable called raw_text
-raw_text = ''
-for i, page in enumerate(doc_reader.pages):
-    text = page.extract_text()
-    if text:
-        raw_text += text
-
-# Splitting up the text into smaller chunks for indexing
-text_splitter = CharacterTextSplitter(
-    separator = "\n",
-    chunk_size = 1000,
-    chunk_overlap  = 200, #striding over the text
-    length_function = len,
-)
-texts = text_splitter.split_text(raw_text)
-
-
-
-# Download embeddings from OpenAI
-import os
-os.environ["OPENAI_API_KEY"] = "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"
-embeddings = OpenAIEmbeddings()
-
-
-docsearch = FAISS.from_texts(texts,embeddings)
-docsearch.embedding_function
-
-
+from langchain_community.vectorstores import FAISS    
 from langchain.chains.question_answering import load_qa_chain
 from langchain_openai import OpenAI
+import os   
 
-chain = load_qa_chain(OpenAI(),
-                      chain_type="stuff") # we are going to stuff all the docs in at once
+def main():
+    st.title("Document Search and Question Answering")
 
+    # File upload widget
+    doc_reader = st.file_uploader("Upload a PDF file", type=["pdf"])
 
+    if doc_reader:
+        raw_text = ''
+        for i, page in enumerate(PdfReader(doc_reader).pages):
+            text = page.extract_text()
+            if text:
+                raw_text += text
 
+        # Splitting up the text into smaller chunks for indexing
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+        )
+        texts = text_splitter.split_text(raw_text)
 
-# query = "what are the content of this document and write each in points?"
-# docs = docsearch.similarity_search(query)
-# x =chain.run(input_documents=docs, question=query)
-# print("query:-->",query)
-# print("ans:-->", x)
+        # Download embeddings from OpenAI
+        os.environ["OPENAI_API_KEY"] = "sk-vhCRSEjDhpwpTd2U4CfT3BlbkFJ1OCSQxoS45pa3IENBXCy"
+        embeddings = OpenAIEmbeddings()
 
-import streamlit as st
+        docsearch = FAISS.from_texts(texts, embeddings)
 
-st.title('DocTalker')
-# Create a text input box for the user
-query = st.text_input('Type your query here...')
+        chain = load_qa_chain(OpenAI(), chain_type="stuff")
 
-# If the user hits enter
-if query:
-        docs = docsearch.similarity_search(query)
-        x =chain.run(input_documents=docs, question=query)
-        st.write(x)
+        query = st.text_input('Type your query here... then press enter')
 
+        if query:
+            docs = docsearch.similarity_search(query)
+            result = chain.run(input_documents=docs, question=query)
+            st.write(result)
 
-# ********* problem and solutions **********
+main()
 
-# FAISS --> works with python 3.9.7  not with 3.10 or 3.11 or 
-# change your python version in vs code by selected it in botton right corner
+# # ********* problem and solutions **********
 
-# how to run
-# create env by python -m venv <name>
-# go to terminal pip install -r doctalk_faiss_requirement.txt
-# download pdf file 
-# first activate environment then type streamlit run try3.py to run this file 
+# # FAISS --> works with python 3.9.7  not with 3.10 or 3.11 or 
+# # change your python version in vs code by selected it in botton right corner
+
+# # how to run
+# # create env by python -m venv <name>
+# # go to terminal pip install -r doctalk_faiss_requirement.txt
+# # download pdf file 
+# # first activate environment then type streamlit run try3.py to run this file 
