9ef5103ca3f2c81d6341709fcf009545804f2851
ishu-knit <123448281+ishu-knit@users.noreply.github.com>
2024-02-14 13:20:04 +0530

    Update doctalk_faiss.py

--- doctalk_faiss.py
+++ doctalk_faiss.py
@@ -32,9 +32,9 @@ def main():
 
         # Download embeddings from OpenAI
         # os.environ["OPENAI_API_KEY"] = "sk-vhCRSEjDhpwpTd2U4CfT3BlbkFJ1OCSQxoS45pa3IENBXCy"
-        OPENAI_API_KEY = "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"
+        OPENAI_API_KEY = "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"
         
-        embeddings = OpenAIEmbeddings(openai_api_key="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")
+        embeddings = OpenAIEmbeddings(openai_api_key="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")
 
         docsearch = FAISS.from_texts(texts, embeddings)
 
